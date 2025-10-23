0ea2887c7b9c23afb1b59abe63e555616fbf4234
Mesrop Janoyan <mesrop.janoyan@slalom.com>
2025-10-23 13:43:21 -0700

    Refactor discovery steps and add comprehensive workshop data

--- script.js
+++ script.js
@@ -5,48 +5,48 @@ const discoverySteps = [
     emoji: "🧭",
     title: "Agreeing On Goals",
     question: "Do we know clearly what our team is working towards?",
-    csvKey: "Goals"
+    csvKey: "goals"
   },
   {
     step: 2,
     emoji: "🧠",
     title: "Understanding The Problem",
     question: "Do we know the biggest problem we are solving?",
-    csvKey: "Understanding"
+    csvKey: "understanding"
   },
   {
     step: 3,
     emoji: "🖼",
     title: "Framing The Problem",
     question: "Are we able to clearly articulate the problem we are solving?",
-    csvKey: "Framing"
+    csvKey: "framing"
   },
   {
     step: 4,
     emoji: "💡",
     title: "Generating Ideas",
     question: "Do we have a range of ideas on how to solve the problem?",
-    csvKey: "Ideating"
+    csvKey: "ideating"
   },
   {
     step: 5,
     emoji: "⚖️",
     title: "Evaluating Solutions",
     question: "Have we assessed if our ideas are robust and viable?",
-    csvKey: "Evaluating"
+    csvKey: "evaluating"
   },
   {
     step: 6,
     emoji: "📌",
     title: "Prioritizing What To Test",
     question: "Have we decided which ideas to test?",
-    csvKey: "Prioritizing"
+    csvKey: "prioritizing"
   },
   {
     step: 7,
     emoji: "🦺",
     title: "Testing A Prototype",
     question: "Have we tested our prototype?",
-    csvKey: "Testing"
+    csvKey: "testing"
   }
 ];
